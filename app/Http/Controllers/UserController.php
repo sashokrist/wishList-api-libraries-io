8c76c15e79a9261a81117ee87f85cfb507d2887c
Alexander <alexander.krist@gmail.com>
2023-04-09 11:23:56 +0300

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
@@ -14,19 +15,20 @@ class UserController extends Controller
     public function storeUser(Request $request)
     {
         try {
+            $user = DB::table('items')->orderBy('id', 'DESC')->first();
             $client = new Client();
-            $result = $client->post('http://127.0.0.1:8000/oauth/token', [
+            $result = $client->post('oauth/token', [
                 'form_params' => [
                     'grant_type' => 'password',
                     'client_id' => '2',
                     'client_secret' => '6JGxEGy9BHcJgMdxaE2Ix0scQ98WTK2clbbTv7Ba',
-                    'username' => 'alexander.krist@gmail.com',
-                    'password' => '12345678',
+                    'username' => $user->email,
+                    'password' => $user->password,
                     'scope' => '',
                 ]
             ]);
             $access_token = json_decode((string) $result->getBody(), true)['access_token'];
-            $result = $client->get('https://myapi.com/client/user', [
+            $result = $client->get('api/user', [
                 'headers' => [
                     'Content-Type' => 'application/json',
                     'Accept' => 'application/json',
